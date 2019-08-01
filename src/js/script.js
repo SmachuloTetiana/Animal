d780d3a9d885ceeb364da4ff339a22126de66d5d
SmachuloTetiana <tanichk@ukr.net>
2019-08-01 11:22:20 +0300

    fix mistakes

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,61 @@
-let breed = document.getElementsByClassName("breed")[0];
+let breed = document.getElementsByClassName("breed")[0],
+    weight = document.getElementsByClassName("weight")[0],
+    eye_color = document.getElementsByClassName("eye_color")[0],
+    life_span = document.getElementsByClassName("life_span")[0],
+    voice = document.getElementsByClassName("voice")[0];
 
-breed.innerHTML = 'Dog breed:';
+// breed.innerHTML = 'Dog breed:';
 
 class Animal {
-
-    constructor(self) {
-        this.self = self;
+    
+    constructor(type) {
+        this.type = type;
     }
 
     getData() {
         axios
             .get('./animal.json')
-            .then(respons => {
-                // this.self = respons.data;
-                // console.log(this.self);
+            .then(response => {
+                // response.data
+                // console.log(this.type);
 
-                if(dog.self === 'Dog') {
-                    this.self = respons.data.dog;
-                    console.log(this.self);
-                } else if (cat.self === 'Cat') {
-                    this.self = respons.data.cat;
-                    console.log(this.self);
+                if(dog.type === 'Dog') {
+                    this.type = response.data.dog;
+                    console.log(this.type);
+                } else if (cat.type === 'Cat') {
+                    this.type = response.data.cat;
+                    console.log(this.type);
                 }
             })
+            // .then(({dog}) => {
+            //     breed.innerHTML = dog.breed;
+            //     weight.innerHTML = dog.weight;
+            //     eye_color.innerHTML = dog.eye_color;
+            //     life_span.innerHTML = dog.life_span;
+            //     voice.innerHTML = dog.voice[Math.floor(Math.random()*dog.voice.length)];
+            // })
     }
 
     
 }
 
 class Dog extends Animal {
-    constructor(self) {
-        super(self)
+    constructor(otherData) {
+        const type = 'Dog';
+        super(type, otherData)
     }
 
 }
 
 class Cat extends Animal {
-    constructor(self) {
-        super(self)
+    constructor(otherData) {
+        const type = 'Cat';
+        super(type, otherData)
     }
 }
 
-let dog = new Dog('Dog');
-let cat = new Cat('Cat');
+let dog = new Dog();
+let cat = new Cat();
 
 dog.getData();
 cat.getData();

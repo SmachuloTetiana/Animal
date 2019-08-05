ff6816f123179a899d12d8cfcdd8dd1817eda787
SmachuloTetiana <tanichk@ukr.net>
2019-08-05 14:26:14 +0300

    add create method

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 (async function() {
     const animalData = await axios.get('./animal.json');
-    let selectItem = document.getElementById("selectItem");
-    
+    let selectItem = document.getElementById("selectItem"),
+        nameAnimal = document.getElementById("name"),
+        btnCreate = document.getElementById("create"),
+        animalImage = document.getElementsByClassName("image")[0],
+        animalName = document.getElementsByClassName("animal__name")[0],
+        animalBreed = document.getElementsByClassName("animal__breed")[0],
+        animalVoice = document.getElementsByClassName("animal__voice")[0];
+
     class Animal {
         constructor(type, additionalData) {
             Object.assign(this, additionalData);
@@ -11,10 +17,23 @@
     
         get voice() {
             return `${this.name} says ${this.data.voice[Math.floor(Math.random()*this.data.voice.length)]}`;
-        }  
+        } 
 
-        createAnimal() {
-            return selectItem.value;
+        create() {  
+            btnCreate.addEventListener('click', function() {
+                var selectVal = selectItem.value;
+                var inputVal = nameAnimal.value;
+  
+                this.data = animalData.data[selectVal];
+                if(this.data) {
+                    animalImage.src = this.data.images[Math.floor(Math.random()*this.data.images.length)];  
+                    animalName.innerHTML = inputVal;
+                    animalBreed.innerHTML = this.data.breed;
+                    animalVoice.innerHTML = this.data.voice[Math.floor(Math.random()*this.data.voice.length)]
+                }
+
+            })   
+                  
         }
     }
     
@@ -34,10 +53,13 @@
     
     let dog = new Dog({ name: 'Alf', color: 'brown' });
     let cat = new Cat({ name: 'Kotik', color: 'black' });
+    // const animal = new Animal();
+    dog.create();
 
     console.log(dog.voice);
     console.log(cat.voice);
-    console.log(dog.createAnimal())
+
+
 })();
 
 
